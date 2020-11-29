d44845852994934a170b327daaed262c1cf004f1
Yufei <zengyf93@qq.com>
2020-11-29 21:58:33 +0800

    refresh

--- workflow_code_log/lanzhou_shotgun_1-2.sh
+++ workflow_code_log/lanzhou_shotgun_1-2.sh
@@ -12,18 +12,35 @@ cd 01_cleandata
 
 ln -s ../00_rawdata/*fq ./
 
-parallel -j 3 --xapply 'echo {1} {2}'  ::: *_R1.fq ::: *_R2.fq
+parallel -j 8 --xapply 'echo {1} {2}'  ::: *_R1.fq ::: *_R2.fq
 
+
+############### trimmomatic and remove host reads
 # 4 thread each job
 parallel -j 8 --xapply 'kneaddata -i {1} -i {2} -o kneaddata_out -v \
- -db $DTB/Human_bowtie2  -t 4 \
+ -db $DTB/Human_bowtie2 \
  --trimmomatic $SGENV/trimmomatic --trimmomatic-options "ILLUMINACLIP:TruSeq3-PE.fa:2:40:15 SLIDINGWINDOW:4:20 MINLEN:50" \
- -t 6 --bowtie2-options "--very-sensitive --dovetail" --remove-intermediate-output' \
+ -t 4 --bowtie2-options "--very-sensitive --dovetail" --remove-intermediate-output' \
   ::: *_R1.fq ::: *_R2.fq
-
 
 # run parallel of each command:
 # trimmomatic PE -phred33 -threads 4 {1} {2} trimmed.{1} trimmed.{2} ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 SLIDINGWINDOW:4:20 MINLEN:50
+# $SGENV/trimmomatic
+parallel -j 14 --xapply 'trimmomatic PE -phred33 -threads 4 {1} {2} \
+      trimmed.{1} outtrimmed.{1} trimmed.{2} outtrimmed.{2}  \
+      ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 \
+      SLIDINGWINDOW:5:20 LEADING:5 TRAILING:5 \
+      MINLEN:50' ::: *_R1.fq ::: *_R2.fq
+
+bowtie2 -p 8 -x GRCh38_noalt_as -1 SAMPLE_R1.fastq.gz -2 SAMPLE_R2.fastq.gz --un-conc-gz
+## --reorder
+bowtie2 -p 8 -x $DTB/Human_bowtie2 -1 trimmed.29_R1.fq -2 trimmed.29_R2.fq -S test_mapped_and_unmapped.sam
+
+parallel -j 3 --xapply 'kneaddata -i {1} -i {2} -o kneaddata_out -v \
+ -db /mnt/f/database/Human_bowtie2 \
+ --bypass-trim  \
+ -t 4 --bowtie2-options "--very-sensitive --dovetail" --remove-intermediate-output' \
+  ::: trimmed.*_R1.fq ::: trimmed.*_R2.fq
 
 rm *unmatch*
 rm *bowtie2*
